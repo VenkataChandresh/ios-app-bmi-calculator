62916b7296f75ddc0270f6a916b383c8ec212987
VenkataChandresh <158313715+VenkataChandresh@users.noreply.github.com>
2025-10-02 09:38:51 -0500

    Initial Commit

---
+++ Adapa_PracticeExam01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Adapa_PracticeExam01
+//
+//  Created by {{partialupn}} on 10/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Adapa_PracticeExam01Tests/Adapa_PracticeExam01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Adapa_PracticeExam01Tests.swift
+//  Adapa_PracticeExam01Tests
+//
+//  Created by {{partialupn}} on 10/2/25.
+//
+
+import Testing
+@testable import Adapa_PracticeExam01
+
+struct Adapa_PracticeExam01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
